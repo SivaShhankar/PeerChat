5d61f2acde579dc4ce58679af39ded18b9741adf
gowrisankar Gnanasekaran <gowrisankar Gnanasekaran>
2016-11-24 13:15:24 +0530

    Peerjs changed

--- Templates/JS/init.js
+++ Templates/JS/init.js
@@ -1,8 +1,1 @@
-/***************************************************************************/
-/*                                                                         */
-/*  This obfuscated code was created by Javascript Obfuscator Free Version.*/
-/*  Javascript Obfuscator Free Version can be downloaded here              */
-/*  http://javascriptobfuscator.com                                        */
-/*                                                                         */
-/***************************************************************************/
-var _$_5330=["rampeerjs.herokuapp.com","443","http://",":","/","open","text","#pid","on","connection","error","log","</br><br/> Please start a new session "];var user;var peerID;var connectedPeers={};var peer,person;var serverIP=_$_5330[0],serverPort=_$_5330[1];var serverHost=_$_5330[2]+ serverIP+ _$_5330[3]+ serverPort+ _$_5330[4];function startPeerSession(){if(person!= null){peer=  new Peer(person,{host:serverIP,port:serverPort,secure:true,debug:3});peer[_$_5330[8]](_$_5330[5],function(a){user= a;peerID= a;$(_$_5330[7])[_$_5330[6]](a)});peer[_$_5330[8]](_$_5330[9],connect);peer[_$_5330[8]](_$_5330[10],function(b){console[_$_5330[11]](b);displayInfo(b+ _$_5330[12],1);return})}}
+var _$_e8f4=["myownpeerjs.herokuapp.com","443","https://",":","/","open","text","#pid","on","connection","error","log","</br><br/> Please start a new session "];var user;var peerID;var connectedPeers={};var peer,person;var serverIP=_$_e8f4[0],serverPort=_$_e8f4[1];var serverHost=_$_e8f4[2]+ serverIP+ _$_e8f4[3]+ serverPort+ _$_e8f4[4];function startPeerSession(){if(person!= null){peer=  new Peer(person,{host:serverIP,port:serverPort,secure:true,debug:3});peer[_$_e8f4[8]](_$_e8f4[5],function(a){user= a;peerID= a;$(_$_e8f4[7])[_$_e8f4[6]](a);updateData();executeQuery()});peer[_$_e8f4[8]](_$_e8f4[9],connect);peer[_$_e8f4[8]](_$_e8f4[10],function(b){console[_$_e8f4[11]](b);displayInfo(b+ _$_e8f4[12],1);return})}}
--- Templates/JS/usersOnline.js
+++ Templates/JS/usersOnline.js
@@ -1,8 +1,1 @@
-/***************************************************************************/
-/*                                                                         */
-/*  This obfuscated code was created by Javascript Obfuscator Free Version.*/
-/*  Javascript Obfuscator Free Version can be downloaded here              */
-/*  http://javascriptobfuscator.com                                        */
-/*                                                                         */
-/***************************************************************************/
-var _$_d13f=["#userList","open","trigger","close","hover",".UserWidget","slideUp","bind","slideDown","ready"];$(document)[_$_d13f[9]](function(){var b=$(_$_d13f[0]);var c;$(_$_d13f[5])[_$_d13f[4]](function(){clearTimeout(c);c= setTimeout(function(){b[_$_d13f[2]](_$_d13f[1])},500)},function(){clearTimeout(c);c= setTimeout(function(){b[_$_d13f[2]](_$_d13f[3])},500)});var a=false;b[_$_d13f[7]](_$_d13f[1],function(){b[_$_d13f[8]](function(){if(!a){executeQuery();a= true}})})[_$_d13f[7]](_$_d13f[3],function(){a= false;b[_$_d13f[6]]()})})
+var _$_14c5=["#userList","open","trigger","close","hover",".UserWidget","slideUp","bind","slideDown","ready"];$(document)[_$_14c5[9]](function(){var b=$(_$_14c5[0]);var c;$(_$_14c5[5])[_$_14c5[4]](function(){clearTimeout(c);c= setTimeout(function(){b[_$_14c5[2]](_$_14c5[1])},500)},function(){clearTimeout(c);c= setTimeout(function(){b[_$_14c5[2]](_$_14c5[3])},500)});var a=false;b[_$_14c5[7]](_$_14c5[1],function(){b[_$_14c5[8]](function(){if(!a){executeQuery();a= true}})})[_$_14c5[7]](_$_14c5[3],function(){a= false;b[_$_14c5[6]]()})})
